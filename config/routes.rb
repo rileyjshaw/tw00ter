f6ca2585f39fbf1f12c8f0d7b692c7b4e8d0a255
Riley Shaw <rileyjshaw@gmail.com>
2013-06-21 11:51:44 -0700

    Add StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Tw00ter::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
