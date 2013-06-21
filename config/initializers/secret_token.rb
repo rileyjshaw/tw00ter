9bfd64369e88c206951f4ef1091c2030c7d9db94
Riley Shaw <rileyjshaw@gmail.com>
2013-06-21 10:55:20 -0700

    Create the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tw00ter::Application.config.secret_token = 'a1f45c1553bdff04918b26819b0f953c675f530981acc9884f2d81774c82081c891babacaaef1a4c89139b5004474030c779a25a15a45badd1c6685bf3374742'
